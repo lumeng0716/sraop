ca49bc3cd08bab737a686f8183c1eb0599e28434
KingCheng <hzchengjinlong@corp.netease.com>
2018-03-22 19:57:26 +0800

    open function callbcak

--- raop.c
+++ raop.c
@@ -118,13 +118,11 @@ conn_init(void *opaque, unsigned char *local, int locallen, unsigned char *remot
 
 	digest_generate_nonce(conn->nonce, sizeof(conn->nonce));
 
-
-/*
 	if(NULL != conn->raop->callbacks.audio_connect)
 	{
          conn->raop->callbacks.audio_connect( conn->raop->callbacks.cls,local,locallen,remote,remotelen);
 	}
-*/
+	
 	return conn;
 }
 
@@ -446,12 +444,12 @@ conn_destroy(void *ptr)
 	free(conn->local);
 	free(conn->remote);
 	free(conn);
-/*
+
 	if(NULL != conn->raop->callbacks.audio_disconnect)
 	{  
 	     conn->raop->callbacks.audio_disconnect(conn->raop->callbacks.cls);
 	}
-	*/
+	
 }
 
 raop_t *
